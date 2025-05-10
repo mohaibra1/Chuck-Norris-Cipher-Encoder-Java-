2a6b4e15398e5fde8f74f43ac4e66524ab06ff63
Moha <mimohadinho@gmail.com>
2025-05-10 16:24:52 +0200

    Stage 1/5: Analyze the words

--- Chuck Norris Cipher Encoder (Java)/task/src/chucknorris/Main.java
+++ Chuck Norris Cipher Encoder (Java)/task/src/chucknorris/Main.java
@@ -1,7 +1,22 @@
 package chucknorris;
 
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+
+        processInput();
+    }
+
+    public static void processInput(){
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Input string:");
+        String word = sc.nextLine();
+        char[] words = word.toCharArray();
+
+        //print out all characters
+        for (char c: words){
+            System.out.print(c + " ");
+        }
     }
 }
